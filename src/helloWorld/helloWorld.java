3aa310bc9e02e7ed79e7bbd07b5dd489204c6aa7
scottyoshimura <scottfiction@yahoo.com>
2016-08-13 23:20:20 -0700

    initial push

---
+++ src/helloWorld/helloWorld.java
@@ -0,0 +1,13 @@
+//a hello world app using JDK 1.8
+//scott t yoshimura
+package helloWorld;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
